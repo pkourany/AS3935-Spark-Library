9754aaa0b37fcd47e8b95b916689a6b393aea89b
Paul Kourany <pkourany@users.noreply.github.com>
2014-04-23 20:08:36 -0400

    Update AS3935.cpp
    Adapted for Spark Core

--- AS3935/AS3935.cpp
+++ AS3935/AS3935.cpp
@@ -15,6 +15,8 @@
   You should have received a copy of the GNU Lesser General Public
   License along with this library; if not, write to the Free Software
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+  
+  Adapted for Spark Core by Paul Kourany, Apr. 23, 2014
 */
 
 #include "AS3935.h"
